5a9d4eb1cec2e34c5a8a6b19fab7010e5cef7fbe
JB <jadon.binda@gmail.com>
2023-09-21 11:08:38 -0300

    refactor: melhoria do código

--- carrossel/script.js
+++ carrossel/script.js
@@ -2,19 +2,13 @@ function carrossel(sequencia) {
   let imagem = 0;
 
   for (let i = 0; i < sequencia.length; i++) {
-    if (sequencia[i] === '>') {
-      imagem++
-    } else if (sequencia[i] === '<') {
-      imagem--
-    }
+    if (sequencia[i] === '>') imagem++
+    else if (sequencia[i] === '<') imagem--
 
-    if (imagem < 0) {
-      imagem = 6;
-    }
+    if (imagem < 0) imagem = 6;
 
     if (imagem > 6) imagem = 0;
   }
-
   return imagem;
 }
 
